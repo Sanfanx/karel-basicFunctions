2aebac7a99cfce54fae638a70085099cacd02cee
Sanfanx <90526566+Sanfanx@users.noreply.github.com>
2024-02-25 08:37:46 -0600

    Update functions.java

--- functions.java
+++ functions.java
@@ -1,8 +1,31 @@
 class program {
     void turnright() {
-      iterate(3){
-        turnleft();
-      }
+        iterate(3){
+            turnleft();
+        }
+    }
+
+    void recoger_todo(pasos){
+        iterate(pasos){
+            while(nextToABeeper()) {
+                pickbeeper();
+            }
+            move();
+        } 
+        if(nextToABeeper()) {
+            pickbeeper();
+        }
+    }
+
+    void dejar_todo(pasos,cuantos){
+        iterate(pasos){
+            iterate(cuantos){
+                if (anyBeepersInBeeperBag){
+                    putbeeper();
+                }
+            }
+            move();
+        } 
     }
 
     void recoger_en_perimetro(longitud) {
@@ -20,7 +43,7 @@ class program {
     void rodear_camino(recoger_beepers) {
         //recoger_beepers = 1; (TRUE)
         //recoger_beepers = 0; (FALSE)
-        turnleft();
+        //turnleft();
         if (rightIsBlocked()){
             while(rightIsBlocked()) {
                 iterate(recoger_beepers){
@@ -63,6 +86,29 @@ class program {
                 turnleft();
             }
         }
+    }
+
+    void salir_laberinto_simple(){
+        while(nextToABeeper()) {
+            
+            if(rightIsBlocked()) {
+                if(frontIsClear()) {
+                    pickbeeper();
+                    move();
+                }
+                else {
+                    turnleft();
+                }
+            }
+            else {
+                turnright();
+                pickbeeper();
+                move();
+            }
+        }
+        turnleft();
+        turnleft();
+        move();
     }
     program () {
         //CODIGOOOOOO :D
